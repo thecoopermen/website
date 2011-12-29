a310d50dbd2b8ca10f23fec3d8430f1e9e8af0be
Ross Cooperman <cooperman@gmail.com>
2011-12-29 15:02:33 -0500

    Added a Rails app that will eventually become the root of the repo (all in good time).

---
+++ railsapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Website::Application.config.secret_token = 'a2a3d5223df711a166b49038fbab18a4d8dccbf79e9fca4ba9b4c905c26228bf5d389c44ce3c067070b6004852853049c344970ec9a09a9aa6903965cfa93202'
