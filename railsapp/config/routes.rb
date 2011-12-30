b90d4e32148904a7c95db3224ec64857dd7149f7
Ross Cooperman <cooperman@gmail.com>
2011-12-30 01:37:15 -0500

    Added a default route that points to posts#index and a super naive app template that is just index.html

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,9 +1,9 @@
 Website::Application.routes.draw do
-  resources :authors
-
   resources :posts, only: [ :index, :show ]
 
   # ActiveAdmin stuff
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  root :to => 'posts#index'
 end
