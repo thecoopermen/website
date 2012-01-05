0e6d4c3e9cc4b67f52d6e9ed9da23ba50e600b53
Ross Cooperman <cooperman@gmail.com>
2012-01-05 00:47:39 -0500

    Removing Ace Editor from the mix -- it just wasn't working well.

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -11,11 +11,11 @@ ActiveAdmin.register Post do
       row('Author') { link_to post.author.name, admin_author_path(post.author) }
       row :title
       row :subtitle
-      row :content
       row :permalink
       row :published_at
       row :updated_at
       row :created_at
+      row('content') { content_tag :pre, post.content }
     end
   end
 
